ff1b0e0eac67f10bbd0ff953e03a28256248bba2
kritsuto11-star <kritsuto11@gmail.com>
2026-08-05 12:20:12 +0900

    Update IItem.cs

--- Assets/005_Kawata/Scripts/IItem.cs
+++ Assets/005_Kawata/Scripts/IItem.cs
@@ -1,7 +1,9 @@
-﻿using UnityEngine;
+﻿using System.Data;
+using UnityEngine;
 
 public interface  IItem 
 {
-
-
+    public void Drop(PlayerScript player);
+    public void Use(PlayerScript player);
+    public void PickUp(PlayerScript player);
 }
