d0aea50965f1a5a25a16622ee41f94ff16607b33
kritsuto11-star <kritsuto11@gmail.com>
2026-08-05 12:10:53 +0900

    keytest

---
+++ Assets/005_Kawata/Scripts/key_have.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class key_ : MonoBehaviour
+{
+    [SerializeField]
+    bool key1 = false;
+    [SerializeField]
+    bool key2 = false;
+    [SerializeField]
+    bool key3 = false;
+
+    private void Start()
+    {
+        if (key1 && key2 && key3)
+        {
+            Debug.Log("扉があいた");
+        }
+    }
+
+}
