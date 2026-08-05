e3f32b9ef3ff3dc5147eaf97782e50a862d2fb37
vantan190509-afk <vantan190509@gmail.com>
2026-08-05 12:41:02 +0900

    add:カメラと移動、ジャンプおわり

--- Assets/003_Ryouta/Scripts/PlayerScript.cs
+++ Assets/003_Ryouta/Scripts/PlayerScript.cs
@@ -4,24 +4,40 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerScript : MonoBehaviour
 {
-    //移動
-     float moveSpeed = 5f;
-     float jumpHeight = 1.5f;
-     float gravity = -20f;
+   
+    [SerializeField] float moveSpeed = 5f;//移動速度
+    [SerializeField] float jumpHeight = 1.5f;//ジャンプの高さ
+    [SerializeField] float gravity = -20f;//重力の強さ
 
     //視点
-     Transform playerCamera;
-     float mouseSensitivity = 0.1f;
-     float lookLimit = 90f;
+     Transform playerCamera;//動かすカメラ
+    [SerializeField, Range(0.01f, 1f)]
+    float CameraSpeed = 0.1f;//カメラの感度
+     float lookLimit = 90f;//カメラの限界角度
 
     CharacterController controller;
     float verticalVelocity;
     float cameraPitch;
     bool cursorLocked = true;
 
-    public void Awake()
+    void Awake()
     {
         controller = GetComponent<CharacterController>();
+
+        if (playerCamera == null)
+            playerCamera = GetComponentInChildren<Camera>()?.transform;
+
+        if (playerCamera == null)
+        {
+            Debug.LogError(
+                "Playerの子にCameraがありません。",
+                this
+            );
+
+            enabled = false;
+            return;
+        }
+
         LockCursor();
     }
 
@@ -43,16 +59,16 @@ public class PlayerScript : MonoBehaviour
         Vector2 input = Vector2.zero;
 
         if (Keyboard.current.wKey.isPressed)
-            input.y += 1f;
+            input.y += 1f;//前移動
 
         if (Keyboard.current.sKey.isPressed)
-            input.y -= 1f;
+            input.y -= 1f;//後ろ移動
 
         if (Keyboard.current.dKey.isPressed)
-            input.x += 1f;
+            input.x += 1f;//右移動
 
         if (Keyboard.current.aKey.isPressed)
-            input.x -= 1f;
+            input.x -= 1f;//左移動
 
         // 斜め移動が速くなることを防ぐ
         input = Vector2.ClampMagnitude(input, 1f);
@@ -88,8 +104,8 @@ public class PlayerScript : MonoBehaviour
 
         Vector2 mouseDelta = Mouse.current.delta.ReadValue();
 
-        float mouseX = mouseDelta.x * mouseSensitivity;
-        float mouseY = mouseDelta.y * mouseSensitivity;
+        float mouseX = mouseDelta.x * CameraSpeed;
+        float mouseY = mouseDelta.y * CameraSpeed;
 
         // 左右はPlayer全体を回す
         transform.Rotate(Vector3.up * mouseX);
@@ -136,16 +152,16 @@ public class PlayerScript : MonoBehaviour
         Cursor.visible = true;
     }
 
-    public void use()//インタラクト
+    public void Use()//使う
     {
 
     }
 
-    public　void Drop()
+    public　void Drop()//落とす
     {
 
     }
-    public void PickUp()
+    public void PickUp()//持つ
     {
 
     }
