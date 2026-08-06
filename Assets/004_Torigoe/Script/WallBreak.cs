a2a537c3b0ebd36d504d2be0a6d0bdcb80949177
Hiragi0000 <vantan.tori@gmail.com>
2026-08-06 15:34:49 +0900

    WallBreak途中

--- Assets/004_Torigoe/Script/WallBreak.cs
+++ Assets/004_Torigoe/Script/WallBreak.cs
@@ -1,16 +1,50 @@
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class WallBreak : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("壁を壊せるアイテムのタグ")]
+    [SerializeField] private string breakItemTag = "CannonBall";
+
+    [Header("破壊アニメーションの長さ")]
+    [SerializeField] private float destroyDelay = 1.2f;
+
+    private Animator animator;
+    private bool isBroken = false;
+
+    private void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        if (isBroken) return;
+
+        if (collision.gameObject.CompareTag(breakItemTag))
+        {
+            isBroken = true;
+
+            // 壁の当たり判定を無効化
+            GetComponent<Collider>().enabled = false;
+
+            // アニメーション再生
+            if (animator != null)
+            {
+                animator.SetTrigger("Break");
+            }
+
+            // 大砲の弾を消す
+            Destroy(collision.gameObject);
+
+            // 壁を削除
+            StartCoroutine(DestroyWall());
+        }
+    }
+
+    IEnumerator DestroyWall()
+    {
+        yield return new WaitForSeconds(destroyDelay);
+        Destroy(gameObject);
     }
 }
