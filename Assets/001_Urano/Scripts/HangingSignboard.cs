eee14d5712ebfb40e896287a80c52cbc42e33935
UranoChikage <uranochikage@gmail.com>
2026-08-06 17:33:52 +0900

    看板作成

---
+++ Assets/001_Urano/Scripts/HangingSignboard.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+/// <summary>紐でぶら下がっている看板。Playerが近づくと落ちてくる</summary>
+public class HangingSignboard : MonoBehaviour
+{
+    [SerializeField] float dropDistance = 3f;
+    [SerializeField] float duration = 1f;
+    [SerializeField] AnimationCurve curve;
+
+    Vector3 startPosition;
+    Vector3 targetPosition;
+    bool triggered;
+    float elapsed;
+
+    void Awake()
+    {
+        startPosition = transform.position;
+        targetPosition = startPosition + Vector3.down * dropDistance;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (triggered) return;
+        if (!other.transform.TryGetComponent<PlayerScript>(out _)) return;
+
+        triggered = true;
+    }
+
+    void Update()
+    {
+        if (!triggered || elapsed >= duration) return;
+
+        elapsed += Time.deltaTime;
+        float t = Mathf.Clamp01(elapsed / duration);
+
+        transform.position = Vector3.LerpUnclamped(startPosition, targetPosition, curve.Evaluate(t));
+    }
+}
