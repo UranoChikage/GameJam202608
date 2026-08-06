3b9152a91312793e47d15e2e5f64e8f4dfd6d9fb
AK080-0 <akito.umezawa824@gmail.com>
2026-08-06 14:11:40 +0900

    Update Taihou.cs

--- Assets/006_Umezawa/Script umezawa/Taihou.cs
+++ Assets/006_Umezawa/Script umezawa/Taihou.cs
@@ -4,8 +4,8 @@ public class Taihou : MonoBehaviour, IInteractable
 {
     public float pow;
     public GameObject prefab;
-    Vector3 Scale = new Vector3(0.1f, 0.1f, 0.1f);  
-    public void Interact()
+    Vector3 Scale = new Vector3(0.1f, 0.1f, 0.1f);
+    public void Interact(PlayerScript player)
     {
         Debug.Log("大砲発射");
         //Gameobjectを生成する
